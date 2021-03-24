fd6326d7f61d604a525d235118a76dacab52f40e
Ramakrishna <rahulsur.suresh@gmail.com>
2021-03-24 19:01:34 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+//import {nameToArray} from './logic'
+
+function App  () {
+
+
+  const [personone, setPersonone] = useState("");
+  const [persontwo, setPersontwo] = useState("");
+  const [result, setResult] = useState("")
+
+
+  const nameToArray = (name) => {
+    let updatedName = name.toUpperCase();
+    
+    updatedName = updatedName.replace(/\s+/g, "");
+    
+    return updatedName.split("");
+  }
+
+  const handleSubmit =  e => {
+    e.preventDefault();
+    setResult("")
+
+    let pOne = nameToArray(personone);
+    let pTwo = nameToArray(persontwo);
+    var flames = ['Friends','Love','Affair','Marriage','Enemy','Sister'];
+    
+    console.log(flames)
+
+    console.log(pOne);
+    console.log(pTwo);
+
+    if (personone.length >0 && persontwo.length >0){
+      var totalNameLength = personone.length+persontwo.length; 
+      
+      while(flames.length !== 1)
+      {
+       flames = flames.splice(totalNameLength%6,1);
+      }
+      console.log(flames)
+    }
+    setResult(flames)
+    //console.log(difference)
+
+  }
+
+  /*
+  let Personone = e.target.elements.name.value;
+  let Persontwo = e.target.elements.name_two.value;
+  */
+
+
+  return (
+    <div className="App">
+      <h1>FLAMES</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" name="name" placeholder="nameone" onChange={e => setPersonone(e.target.value)} />
+        </label>
+        <label>
+          <input type="text" name="name_two" placeholder="nametwo" onChange={e => setPersontwo(e.target.value)} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+        </form>
+        <br></br>
+        <h3>{result}</h3>
+    </div>
+
+  );
+}
+
+export default App;
